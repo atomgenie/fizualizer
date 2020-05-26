fdc6e388db1d009bb32d4c630e4ae40b6041118e
Tâm-Tanguy TRAN <thanh-tam-tangu.tran@epita.fr>
2020-05-26 14:53:28 +0200

    feat: Add page

---
+++ docs/precache-manifest.0c3c7b619bae7e351cf398bb793db83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1635f58958a7bd1d5cc4630b92f92dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f0b13b59589fc2b25ba",
+    "url": "/static/css/main.7a1fad05.chunk.css"
+  },
+  {
+    "revision": "c1efdfc637caacdc19c5",
+    "url": "/static/js/2.a51881bd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a51881bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f0b13b59589fc2b25ba",
+    "url": "/static/js/main.ffb75ba2.chunk.js"
+  },
+  {
+    "revision": "6edadc8e910f66da0a87",
+    "url": "/static/js/runtime-main.eddb7cb4.js"
+  }
+]);
